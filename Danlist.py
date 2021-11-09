3aba15a83318cbc317e05e66c323fb1c1ce7a4be
cjrzs <wawy826442063@163.com>
2021-11-10 00:03:36 +0800

    refactor: 结构更新

--- Danlist.py
+++
@@ -1,9 +0,0 @@
-Listvalue = [1,5,6,2,4,3]
-ListPointer = [3,2,-1,5,1,4]
-head = 0
-print(Listvalue[head])
-next = ListPointer[head]
-while next!=-1:
-    print(Listvalue[next])
-    next = ListPointer[next]
-
--- Danlist2.py
+++
@@ -1,9 +0,0 @@
-LinkedList = [[1,3],[5,2],[6,-1],[2,5],[4,1],[3,4]]
-value = 0
-pointer = 1
-head = 0
-print(LinkedList[head][value])
-next = LinkedList[head][pointer]
-while next!= -1:
-    print(LinkedList[next][value])
-    next = LinkedList[next][pointer]
